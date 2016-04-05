9c91e79a79a2c32fea38647c51ad6d9edb28f4df
Chris Mytton <chrismytton@gmail.com>
2016-04-05 17:28:57 +0200

    Add support for Alcalde in the _plugin

--- _plugins/yournextrepresentative.rb
+++ _plugins/yournextrepresentative.rb
@@ -6,6 +6,13 @@ class YNR < Jekyll::Generator
       'filters' => {
         'elected' => 'True'
       }
+    },
+    {
+      'source' => 'http://tusrepresentanteslocales.co.cr/media/candidates-mun-al-2016.csv',
+      'collection_name' => 'alcades',
+      'filters' => {
+        'elected' => 'True'
+      }
     }
   ]
 
@@ -13,25 +20,31 @@ class YNR < Jekyll::Generator
     SOURCES.each do |csv|
       csv['filters'] ||= {}
       Jekyll::Csv::CollectionPopulator.new(csv).populate(site)
-      site.collections[collection].docs = site.collections[collection].docs.find_all do |doc|
+      site.collections[csv['collection_name']].docs = site.collections[csv['collection_name']].docs.find_all do |doc|
         csv['filters'].all? { |k, v| doc.data[k] == v }
       end
     end
 
     cantons = Jekyll::Collection.new(site, 'cantons')
-    site.collections['regidores'].docs.group_by { |r| r['post_label'] }.each do |post_label, regidores|
-      canton_name = post_label.sub(/^Regidor de /, '')
+    alcades_cantons = site.collections['alcades'].docs.group_by { |r| r['post_label'].sub(/^Alcalde de /, '') }
+    regidores_cantons = site.collections['regidores'].docs.group_by { |r| r['post_label'].sub(/^Regidor de /, '') }
+    canton_names = (alcades_cantons.keys + regidores_cantons.keys).uniq.sort
+    canton_names.each do |canton_name|
       path = File.join(site.source, "_cantons", "#{Jekyll::Utils.slugify(canton_name)}.md")
       canton = Jekyll::Document.new(path, collection: cantons, site: site)
+      alcades = alcades_cantons[canton_name].to_a
+      regidores = regidores_cantons[canton_name].to_a
+      warn canton_name if alcades.any? && regidores.any?
       canton.merge_data!(
         'name' => canton_name,
+        'alcades' => alcades,
         'regidores' => regidores
       )
       if site.layouts.key?('cantons')
         canton.merge_data!('layout' => 'cantons')
       end
       cantons.docs << canton
-      regidores.each { |r| r.data['canton'] = canton }
+      (alcades + regidores).each { |r| r.data['canton'] = canton }
     end
     site.collections['cantons'] = cantons
   end
